d27c91da35e07fe90fb868ac009b7166f5839fcb
muryhurris <mary.harris2@g.austincc.edu>
2021-02-17 16:24:23 -0600

    Homework 3: If-else and Switch functions

--- src/classwork/03_assign/decision.cpp
+++ src/classwork/03_assign/decision.cpp
@@ -1,1 +1,59 @@
 //cpp
+#include "decision.h"
+
+string get_letter_grade_using_if(int grade)
+{
+    if(grade >= 90 && grade <= 100)
+    {
+        return "A";
+    }
+    else if(grade >= 80 && grade <= 89)
+    {
+        return "B";
+    }
+    else if(grade >= 70 && grade <= 79)
+    {
+        return "C";
+    }
+    else if(grade >= 60 && grade <= 69)
+    {
+        return "D";
+    }
+    else if(grade >= 0 && grade <= 59)
+    {
+        return "F";
+    }
+    else
+    {
+        return "Number out of range.";
+    }
+    return 0;
+}
+
+string get_letter_grade_using_switch(int grade)
+{
+    string result; 
+
+    switch(grade)
+    {
+        case 90 ... 100:
+            result = "A";
+            break;
+         case 80 ... 89:
+            result = "B";
+            break;
+          case 70 ... 79:
+            result = "C";
+            break;
+         case 60 ... 69:
+            result = "D";
+            break;
+         case 0 ... 59:
+            result = "F";
+            break;
+         default:
+            result = "Number out of range.";
+        
+    }
+        return result;
+}
--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -1,9 +1,29 @@
 //Write the include statement for decisions.h here
+#include "decision.h"
+
 
 //Write namespace using statements for cout and cin
+#include<iostream>
+#include<string>
+using std::cout; using std::cin; using std::string;
+
 
 int main() 
 {
+	int grade;
+	
+	//Displays the letter grade using the 'switch' function
+	cout<<"Enter numerical grade value: ";
+	cin>>grade;
+
+	string result = get_letter_grade_using_switch(grade);
+	cout<<"Your letter grade is: "<<result<<"\n";
+	
+	//Displays the letter grade using the 'if' function
+	cout<<"Enter numerical grade value: ";
+	cin>>grade;
+	string letter_grade = get_letter_grade_using_if(grade);
+	cout<<"Your letter grade is: "<<letter_grade;
 
 	return 0;
 }
